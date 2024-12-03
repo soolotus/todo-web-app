b7d363764619dd724ea437af26428cc6359913c4
SooYeon Seong <soolotus632@gmail.com>
2024-12-02 21:33:02 -0700

    Change subheader

--- web.py
+++ web.py
@@ -9,7 +9,7 @@ def add_todo():
 
 
 st.title("My Todo App")
-st.subheader("This is may todo app.")
+st.subheader("This is my todo app.")
 st.write("This app is to increase your productivity")
 
 for index, todo in enumerate(todos):
